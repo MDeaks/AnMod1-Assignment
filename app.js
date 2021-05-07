058e69b9474050f2a44ddcd59159ac3a6976374b
MDeaks <mark.deakins72@gmail.com>
2021-05-07 14:47:14 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('LunchController', LunchController);
+
+LunchController.$inject = ['$scope'];
+
+function LunchController($scope) {
+  $scope.LunchItems = "";
+  $scope.LunchMessage="";
+  
+  $scope.CheckItems = function(){
+  
+    var items = $scope.LunchItems.split(',');
+    if ( $scope.LunchItems===""){
+      $scope.LunchMessage="Please enter data first";
+    }   
+    else if(items.length >=1 && items.length<=3){
+      $scope.LunchMessage="Enjoy";
+    }
+    else {
+      $scope.LunchMessage="Too much!";
+    }    
+
+    console.log(items.length);
+  };
+ 
+}
+})();
